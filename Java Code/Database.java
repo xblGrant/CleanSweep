eabeaec12c9a6e1ab3f1125ed93f820cbb802e7f
Sperry32 <stupac31@gmail.com>
2018-02-14 16:41:32 -0500

    Add files via upload
    Added base code for Database, still will need jdbc link and statements written

---
+++ Java Code/Database.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class Database {
+	static final String DATABASE_URL = "jdbc:ucanaccess://";
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet rSet = null;
+	
+	
+}
