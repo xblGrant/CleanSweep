f8278eb5aa9e25e3610da501a273c1609bb71c1b
Christian Brand <christian12950@gmail.com>
2018-02-19 17:39:47 -0500

    testing
    testing

--- Java Code/Main.java
+++ Java Code/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world");
         Employee bruce = new Employee("Bruce", "psswrd");
         System.out.println(bruce.getName());
         System.out.println(bruce.getPassword());
