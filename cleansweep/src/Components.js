6e96d905cc9014e2e6735bd8807cca4fa6c0c224
kaweldon <kaweldon@github.com>
2018-02-19 20:05:15 -0500

    Component Code

---
+++ cleansweep/src/Components.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function HelloUser(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+class LogButton extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {isLoggedIn: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(prevState => ({
+            isLoggedIn: !prevState.isLoggedIn
+        }));
+    }
+
+    render() {
+        return(
+            <button onClick={this.handleClick}>
+                {this.state.isLoggedIn ? 'Login' : 'Logout'}
+            </button>
+        );
+    }
+}
