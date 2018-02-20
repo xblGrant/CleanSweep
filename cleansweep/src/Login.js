3fca9d2f6c2fa649e01a1aa73fa3fb26d73c3460
kaweldon <kaweldon@github.com>
2018-02-20 14:54:42 -0500

    Updated Login Code
    compartmentalized the code for reusability, set up a listener on the login button.  Need to implement listener code.

--- cleansweep/src/Login.js
+++ cleansweep/src/Login.js
@@ -1,50 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Login extends React.Component {
+
+class LoginButton extends React.Component {
     constructor(props) {
         super(props);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
+    handleLogin() {
+        // Handle login here
+    }
 
     render() {
         return (
-            <div className="App">
-                <head>
-                    <title>Clean Sweep</title>
-                    <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-                </head>
-                <h2>Login Page</h2>
-                <div className="container">
-                    <form>
-                        <div className="row">
-                            <div className="four columns">
-                                <label>User Name: </label>
-                            </div>
-                            <div className="eight columns">
-                                <input name="username" id="username"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="four columns">
-                                <label>Password: </label>
-                            </div>
-                            <div className="eight columns">
-                                <input name="userpass" id="userpass" type="password"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="twelve columns">
-                                <button className="btn btn-default" type="submit">Log In</button>
-                            </div>
-                        </div>
-
-                    </form>
+            <div className="row">
+                <div className="twelve columns">
+                    <button onClick={this.handleLogin} className="btn btn-default" type="submit">Login</button>
                 </div>
             </div>
-            < /div>
         );
     }
+
 }
+
+function InputField(props) {
+    InputField.defaultProps = {
+        display: "Input: ",
+        type: "text"
+    };
+
+    return (
+        <div className="row">
+            <div className="four columns">
+                <label>{props.display}</label>
+            </div>
+            <div className="eight columns">
+                <input name={props.id} id={props.id} type={props.type}/>
+            </div>
+        </div>
+    );
+}
+
+function LoginForm() {
+    return (
+        <form>
+            <InputField display={"Username:"} id={"username"}/>
+            <InputField display={"Password:"} id={"userpass"} type={"password"}/>
+            <LoginButton />
+        </form>
+    );
+}
+
+function Login() {
+    return (
+        <div className="App">
+            <head>
+                <title>Clean Sweep</title>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </head>
+            <h2>Login Page</h2>
+            <div className="container">
+                <LoginForm />
+            </div>
+        </div>
+    );
+}
+
+InputField.propTypes = {
+    display: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
+};
+
 export default Login;
