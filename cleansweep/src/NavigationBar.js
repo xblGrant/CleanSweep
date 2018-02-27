393e201b4e879cd0d2644920e582dd9b065d689e
Christian Brand <christian12950@gmail.com>
2018-02-27 15:29:46 -0500

    bug fix

--- cleansweep/src/NavigationBar.js
+++ cleansweep/src/NavigationBar.js
@@ -13,11 +13,9 @@ import {
     DropdownItem } from 'reactstrap';
 import Header from './Header';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
+
 import CheckInGuest from "./CheckInGuest";
 import InspectRoom from "./InspectRoom";
-=======
->>>>>>> 2ba7dbbeaf20de299ca74a25341c988cbd18b4ea
 
 class NavigationBar extends React.Component {
     constructor(props) {
@@ -46,13 +44,10 @@ class NavigationBar extends React.Component {
                         <NavbarBrand href="/">CleanSweep</NavbarBrand>
                         <Collapse isOpen={this.state.isOpen} navbar>
                                 <Nav className="mr-auto" navbar>
-<<<<<<< HEAD
                                     <DropDContent title={"File"} contents={["New Employee", "New Room"]} links={["/NewEmployee", "NewRoom"]} />
                                     <DropDContent title={"Functions"} contents={["Add Wake-Up Call", "Add Incident", "Assign Rooms", "Inspect Room", "Check-in Guest", "Check-out Guest"]} links={["#","#","#","InspectRoom","CheckInGuest","#"]} />
-=======
                                     <DropDContent title={"File"} contents={["New Employee", "New Room"]} links={["/NewEmployee", "/NewRoom"]} />
                                     <DropDContent title={"Functions"} contents={["Add Wake-Up Call", "Add Incident", "Assign Rooms", "Inspect Room", "Check-in Guest", "Check-out Guest"]} links={["#","#","#","#","/CheckInGuest","#"]} />
->>>>>>> 2ba7dbbeaf20de299ca74a25341c988cbd18b4ea
                                     <DropDContent title={"Lists"} contents={["Assigned Rooms", "Available Rooms", "Inspections", "Incidents", "Wake-Up Calls", "Departing Guests"]} links={["#","#","#","#","#","#"]} />
                                     <DropDContent title={"Options"} contents={["Option1", "Option2"]} links={["#", "#"]}/>
                                     <NavItem>
