193045a5d8d009f53accac46e2821eb90dc053dd
Grant Abbondanza <35235145+xblGrant@users.noreply.github.com>
2018-02-26 20:15:53 -0500

    started react Router for index

--- cleansweep/src/index.js
+++ cleansweep/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+)
 registerServiceWorker();
