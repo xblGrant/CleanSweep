2cf347cd18f2f8d7c9ebb97262abcd41fe35167d
Grant Abbondanza <35235145+xblGrant@users.noreply.github.com>
2018-02-26 20:57:03 -0500

    newEmployee component

---
+++ cleansweep/src/NewEmployee.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Form, FormGroup, Label, Input, FormText, Col} from 'reactstrap';
+
+class NewEmployee extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <Form>
+                <FormGroup row>
+                    <Label for="employeeFName">First Name</Label>
+                    <Col sm={10}>
+                        <Input type="textarea" name="textarea" id="employeeFName"/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="employeeLName">Last Name</Label>
+                    <Col sm={10}>
+                        <Input type="textarea" name="textarea" id="employeeLName"/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="exmployeeDOB">Date Of Birth</Label>
+                    <Col sm={10}>
+                        <Input type="date" name="date" id="exmployeeDOB"/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="employeePass">Password</Label>
+                    <Col sm={10}>
+                        <Input type="password" name="password" id="employeePass"/>
+                    </Col>
+                </FormGroup>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="checkbox" name="checkbox" id="isManager"/>{' '}
+                        Manager
+                    </Label>
+                </FormGroup>
+            </Form>
+        );
+    }
+
+}
+export default NewEmployee;
--- cleansweep/src/index.js
+++ cleansweep/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import NewEmployee from './NewEmployee';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -10,6 +11,7 @@ ReactDOM.render(
     <Router>
         <div>
             <Route exact path="/" component={App} />
+            <Route path="/newEmployee" component={NewEmployee} />
         </div>
     </Router>,
     document.getElementById('root')
