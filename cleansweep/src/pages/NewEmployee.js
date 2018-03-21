c2679d5a89e18fd2f4ba76178a0602bf39935f99
Adina <italian.chicken.salads@gmail.com>
2018-03-21 12:16:01 -0400

    New Employee css
    New Employee- Changed margin to col-sm but it refused to center

--- cleansweep/src/pages/NewEmployee.js
+++ cleansweep/src/pages/NewEmployee.js
@@ -17,40 +17,50 @@ class NewEmployee extends React.Component {
 
     render() {
         return (
-            <div>
+            <div classname={"container"}>
                 <head>
                     <title>New Employee</title>
                 </head>
                 <div id={"newEmployeeForm"}>
                     <Form>
                         <FormGroup row>
-                            <div className={"col-sm-4 center"}>
-                            <Label className={"col-sm-4"} for="employeeFName">First Name</Label>
-                            <Input type="text" className={"col-sm-10 center"} id="employeeFName" placeholder={"First name"}/>
+                            <div classname={"row"}>
+                                <div className={"col-sm-10 center"}>
+                                <Label for="employeeFName">First Name</Label>
+                                <Input type="text" className={"col-sm-12"} id="employeeFName" placeholder={"First name"}/>
+                                </div>
                             </div>
                         </FormGroup>
                         <FormGroup row>
-                            <div className={"col-sm-4 center"}>
-                            <Label className={"col-sm-4"} for="employeeLName">Last Name</Label>
-                            <Input type="text" className={"col-sm-10 center"} id="employeeLName" placeholder={"Last name"}/>
+                            <div classname={"row"}>
+                                 <div className={"col-sm-10 center"}>
+                                <Label for="employeeLName">Last Name</Label>
+                                <Input type="text" className={"col-sm-12"} id="employeeLName" placeholder={"Last name"}/>
+                                </div>
                             </div>
                         </FormGroup>
                         <FormGroup row>
-                            <div className={"col-sm-4 center"}>
-                            <Label className={"col-sm-4"} for="employeeDOB">Date Of Birth</Label>
-                            <Input type="date" className={"col-sm-10 center"} id="employeeDOB"/>
+                            <div classname={"row"}>
+                                <div className={"col-sm-10 center"}>
+                                <Label for="employeeDOB">Date Of Birth</Label>
+                                <Input type="date" className={"col-sm-12"} id="employeeDOB"/>
+                                </div>
                             </div>
                         </FormGroup>
                         <FormGroup row>
-                            <div className={"col-sm-4 center"}>
-                            <Label className={"col-sm-4"} for="employeePass">Email</Label>
-                            <Input type="email" className={"col-sm-10 center"} id="employeeEmail" placeholder={"Email"}/>
+                            <div classname={"row"}>
+                                <div className={"col-sm-10 center"}>
+                                <Label for="employeePass">Email</Label>
+                                <Input type="email" className={"col-sm-12"} id="employeeEmail" placeholder={"Email"}/>
+                                </div>
                             </div>
                         </FormGroup>
                         <FormGroup row>
-                            <div className={"col-sm-4 center"}>
-                            <Label className={"col-sm-4"} for="employeePass">Password</Label>
-                            <Input type="password" className={"col-sm-10 center"} id="employeePass" placeholder={"Password"}/>
+                            <div classname={"row"}>
+                                <div className={"col-sm-10 center"}>
+                                <Label for="employeePass">Password</Label>
+                                <Input type="password" className={"col-sm-12"} id="employeePass" placeholder={"Password"}/>
+                                </div>
                             </div>
                         </FormGroup>
                         <FormGroup check>
@@ -62,10 +72,12 @@ class NewEmployee extends React.Component {
                             </div>
                         </FormGroup>
                         <br/>
-                        <div className={"col-sm-4 center"}>
-                            <Button className={"col-sm-4"} onClick={this.handleNewEmployee} color={"primary"}>Submit</Button>
-                            {' '}
-                            <Button className={"col-sm-4"} link={routes.HOME}>Cancel</Button>
+                        <div classname={"row"}>
+                            <div className={"col-sm-4 center"}>
+                                <Button className={"col-sm-4"} onClick={this.handleNewEmployee} color={"primary"}>Submit</Button>
+                                {' '}
+                                <Button className={"col-sm-4"} link={routes.HOME}>Cancel</Button>
+                            </div>
                         </div>
                 </Form>
                 </div>
