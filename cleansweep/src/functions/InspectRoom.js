4e38254cb8c3a636a2aa0dfc8e743371996b8ede
Grant Abbondanza <35235145+xblGrant@users.noreply.github.com>
2018-04-02 21:20:50 -0400

    Inspect room
    some functionality to inspect room.
    added button to switch between reservable and non-reservable rooms
    
    not complete.

--- cleansweep/src/functions/InspectRoom.js
+++ cleansweep/src/functions/InspectRoom.js
@@ -17,14 +17,40 @@ class InspectRoom extends React.Component {
 
         this.state = {
             rooms: [],
+            selectedRoom: null,
+            areReservableRooms: false
         };
 
         this.handleInspect = this.handleInspect.bind(this);
+        this.handleDecline = this.handleDecline.bind(this);
+        this.handleSelectedRoom = this.handleSelectedRoom.bind(this);
         this.handleFloorSelect = this.handleFloorSelect.bind(this);
     }
 
     componentDidMount() {
         api.getListofAllRoomsNeedInspected(this);
+    }
+
+    handleRoomType() {
+        this.setState({
+            areReservableRooms: !this.state.areReservableRooms,
+            selectedRoom: null
+        });
+
+        let {areReservableRooms} = this.state;
+        let floor = document.getElementById('floorSelect').value;
+
+        if (areReservableRooms) {
+            if (floor === '000')
+                api.getListofAllReservableRooms(this);
+            else
+                api.getListofAllReservableRoomsByFloor(this, floor);
+        } else {
+            if (floor === '000')
+                api.getListofAllNonReservableRooms(this);
+            else
+                api.getListofAllNonReservableRoomsByFloor(this, floor);
+        }
     }
 
     handleFloorSelect(e) {
@@ -34,8 +60,21 @@ class InspectRoom extends React.Component {
             api.getListofAllRoomsNeedInspectedByFloor(this, e.target.value);
     }
 
+    handleSelectedRoom(e) {
+        let room = e.target.value;
+        if (room === '') {room = null}
+        this.setState({
+            selectedRoom: room
+        })
+    }
+
     handleInspect() {
-        // TODO: implement
+        // TODO: implement when inspection is approved
+    }
+
+    handleDecline() {
+        // TODO: implement when inspection is declined
+        // TODO: should set the room status = dirty, isReservable = false,
     }
 
     render() {
@@ -53,6 +92,14 @@ class InspectRoom extends React.Component {
                             </Input>
                         </div>
                     </FormGroup>
+
+                    <FormGroup check>
+                        <Label className={"margin-left-35"} check>
+                            <Input onChange={this.handleRoomType} type={"checkbox"} id={"isNonReservable"}/>{' '}
+                            Non-Reservable Rooms
+                        </Label>
+                    </FormGroup>
+
                     <FormGroup row>
                         <div className={"col-sm-4 center"}>
                             <Label for="assignableRoom">Rooms</Label>
@@ -61,17 +108,12 @@ class InspectRoom extends React.Component {
                             </Input>
                         </div>
                     </FormGroup>
-                    <FormGroup row>
-                        <div className={"col-sm-4 center"}>
-                            <Label for="inspectComment">Comment</Label>
-                            <Input type="textarea" placeholder={"Enter comment here"}/>
-                        </div>
-                    </FormGroup>
 
                     <br/>
                     <div className={"row"}>
                         <div className={"col-sm-5 center"}>
-                            <Button className={"col-sm-4"}onClick={this.handleInspect} color={"primary"}>Submit</Button>
+                            <Button className={"col-sm-4"} onClick={this.handleInspect} color={"primary"}>Approve</Button>
+                            <Button className={"col-sm-4"} onClick={this.handleDecline} color={"danger"}>Decline</Button>
                             <Button className={"col-sm-4"} href={routes.HOME}> Cancel </Button>
                         </div>
                     </div>
