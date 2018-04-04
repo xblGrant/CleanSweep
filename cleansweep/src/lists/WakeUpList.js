7408b7ef63db6559809928e07c831c7181286fc4
kaweldon <kaweldon@github.com>
2018-04-04 13:25:07 -0400

    updated WakeUpList

--- cleansweep/src/lists/WakeUpList.js
+++ cleansweep/src/lists/WakeUpList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Form, FormGroup, Label} from 'reactstrap';
-import GroupSelect from '../selectable/GroupSelect';
+import {Form, FormGroup} from 'reactstrap';
 import * as api from '../firebase/api';
 import {Helmet} from "react-helmet";
 import * as routes from "../constants/routes";
+import * as constant from '../constants/constants';
 
 class WakeUpList extends React.Component {
     constructor(props) {
@@ -14,25 +14,10 @@ class WakeUpList extends React.Component {
             rooms: [],
             selectedRooms: null,
         };
-
-        this.handleSelectionClear = this.handleSelectionClear.bind(this);
-        this.handleSelectionFinish = this.handleSelectionFinish.bind(this);
     }
 
     componentDidMount() {
         api.getRoomsWithWakeUpCalls(this);
-    }
-
-    handleSelectionFinish = selectedItems => {
-        let selectedRooms = [];
-        for (let i = 0; i < selectedItems.length; i++)
-            selectedRooms[i] = selectedItems[i].props;
-
-        this.setState({selectedRooms: selectedRooms});
-    };
-
-    handleSelectionClear() {
-        this.setState({selectedRooms: null})
     }
 
     render() {
@@ -45,16 +30,7 @@ class WakeUpList extends React.Component {
                 <div>
                     <Form>
                         <FormGroup>
-                            <div className={"container text-center"}>
-                                <Label className={"header"}>Wake-Up Calls by Room</Label>
-                            </div>
-                            <GroupSelect items={this.state.rooms}
-                                         isDisabled={false}
-                                         onSelectionClear={this.handleSelectionClear}
-                                         onSelectionFinish={this.handleSelectionFinish}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <WakeUpComponent rooms={this.state.selectedRooms}/>
+                            <WakeUpComponent rooms={this.state.rooms}/>
                         </FormGroup>
                     </Form>
                 </div>
@@ -71,20 +47,20 @@ const WakeUpComponent = ({rooms}) => (
             <th>Time</th>
         </tr>
         {((rooms === null)) ? null : rooms.map((room) => (
-            <WakeUpElement room={room}/>  ))}
+            <WakeUpElement room={room}/> ))}
     </table>
 );
 
 function WakeUpElement(props) {
     let room = props.room;
-    let parts = room.wakeupcall.split(' - ');
+    let parts = room[constant.WAKE_UP_CALL].split(' - ');
     let date = parts[0], time = parts[1];
 
     return (
         <tr>
             <td>
-                <Link to={routes.RESERVABLE_ROOM + room.roomName}>
-                    {room.roomName}</Link>
+                <Link to={routes.RESERVABLE_ROOM + room[constant.ROOM]}>
+                    {room[constant.ROOM]}</Link>
             </td>
             <td>{date}</td>
             <td>{time}</td>
