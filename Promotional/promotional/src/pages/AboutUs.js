ad6150b9014e7b9912c00ebb3e4963fc4e3b4aa2
Adina <italian.chicken.salads@gmail.com>
2018-04-15 18:06:54 -0400

    about us

--- Promotional/promotional/src/pages/AboutUs.js
+++ Promotional/promotional/src/pages/AboutUs.js
@@ -25,7 +25,31 @@ class About extends React.Component {
                 <div className={"container"}>
                     <div className={"col-sm-6  col-md-10 center"}>
                         <h2 className={"center"}>About Us</h2>
+                        <h3>Grant Abbondanza</h3>
+                            <p>
 
+                            </p>
+                        <h3>Christian Brand</h3>
+                            <p>
+
+                            </p>
+                        <h3>Adina Lamboy</h3>
+                            <p>
+                                Currently a Senior at Penn State Harrisburg persuing a
+                                Bachelors Degree in Computer science. 
+                            </p>
+                        <h3>Stuart Perry</h3>
+                            <p>
+
+                            </p>
+                        <h3>James Ringler</h3>
+                            <p>
+
+                            </p>
+                        <h3>Kyle Weldon</h3>
+                            <p>
+
+                            </p>
                     </div>
                 </div>
             </div>
